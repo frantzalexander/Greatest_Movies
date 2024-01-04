92ecb01d4dbbd9a0d60ee50ba844d06b561090e7
Frantz Alexander <mr.frantz.alexander@gmail.com>
2024-01-04 08:34:54 -0500

    completed program file

--- main.py
+++ main.py
@@ -10,4 +10,26 @@ response.raise_for_status()
 webpage = response.text
 soup = BeautifulSoup(webpage, "html.parser")
 
-print(soup.prettify())
+movie_list = [movie.getText() for movie in soup.find_all(
+    name = "h3",
+)]
+
+reverse_movie_list = movie_list[::-1]
+
+with open("movie_list.txt", mode = "w") as file:
+    for movie in reverse_movie_list:
+        file.write(f"{movie}\n")
+        
+def movie_ranking():
+    user_input = int(input("Position of movie: \n"))
+    
+    if user_input <= 100:
+        print(f"The movie ranked {user_input} was: {reverse_movie_list[user_input][3:]}.")
+    
+    else:
+        print("The movie ranking shows the greatest 100 movie.")
+        print("Please choose a ranking between 1 and 100.")
+
+movie_ranking()
+    
+    
